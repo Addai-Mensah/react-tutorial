a19c07ba92a2d06b4b934657ed9cf63850a26c2b
Addai-Mensah <mensahaddaai@gmail.com>
2022-09-04 16:22:27 +0100

    push

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/home'
 import Shop from "./pages/shop"
 import Carts from "./pages/carts"
 import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
 
 
 function App() {
